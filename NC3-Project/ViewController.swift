f9073499ff0319a935e3bb3861370ae15d0936bf
Windy <windywu812@gmail.com>
2020-08-29 20:05:39 +0700

    Initial Commit

---
+++ NC3-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC3-Project
+//
+//  Created by Windy on 29/08/20.
+//  Copyright © 2020 Windy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
